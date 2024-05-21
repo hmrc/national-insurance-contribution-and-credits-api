fe875c920eb94b56e875a2c1ba1d4a6efddba4ea
jacobwestwell <89075596+jacobwestwell@users.noreply.github.com>
2024-05-21 14:46:56 +0100

    Added connector and refactored

--- test/uk/gov/hmrc/bereavementsupportpaymentapi/controllers/ServiceControllerSpec.scala
+++ test/uk/gov/hmrc/bereavementsupportpaymentapi/controllers/ServiceControllerSpec.scala
@@ -6,14 +6,14 @@ import play.api.http.Status
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Helpers}
 
-class MicroserviceHelloWorldControllerSpec extends AnyWordSpec with Matchers {
+class ServiceControllerSpec extends AnyWordSpec with Matchers {
 
   private val fakeRequest = FakeRequest("GET", "/")
-  private val controller = new MicroserviceHelloWorldController(Helpers.stubControllerComponents())
+  private val controller = new ServiceController(Helpers.stubControllerComponents())
 
   "GET /" should {
     "return 200" in {
-      val result = controller.hello()(fakeRequest)
+      val result = controller.getNinoInfo()(fakeRequest)
       status(result) shouldBe Status.OK
     }
   }
