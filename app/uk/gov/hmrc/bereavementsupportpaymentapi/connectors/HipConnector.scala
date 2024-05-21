1c7c00ae3fad1ebdda55fa1bfdc9987fe489e850
jacobwestwell <89075596+jacobwestwell@users.noreply.github.com>
2024-05-21 14:46:56 +0100

    Added connector and refactored

--- app/uk/gov/hmrc/bereavementsupportpaymentapi/connectors/HipConnector.scala
+++ app/uk/gov/hmrc/bereavementsupportpaymentapi/connectors/HipConnector.scala
@@ -14,18 +14,12 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.bereavementsupportpaymentapi.controllers
+package uk.gov.hmrc.bereavementsupportpaymentapi.connectors
 
-import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.Future
+class HipConnector {
 
-@Singleton()
-class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
-    extends BackendController(cc) {
-
-  def hello(): Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok("Hello world"))
+  def getNinoInfo(): String = {
+    "From the connector"
   }
+
 }
