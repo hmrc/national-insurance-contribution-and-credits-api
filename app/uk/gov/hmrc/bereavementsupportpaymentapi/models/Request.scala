19f20e8fe11a127e1027e4067f8c19eaf79f6fa2
martin-adaramola <169259944+martin-adaramola@users.noreply.github.com>
2024-05-28 10:14:22 +0100

    added progress on error handling of parameter validation

--- app/uk/gov/hmrc/bereavementsupportpaymentapi/models/Request.scala
+++ app/uk/gov/hmrc/bereavementsupportpaymentapi/models/Request.scala
@@ -16,11 +16,35 @@
 
 package uk.gov.hmrc.bereavementsupportpaymentapi.models
 
+import uk.gov.hmrc.bereavementsupportpaymentapi.utils.AdditionalHeaderNames
+
 import java.time.LocalDate
+import java.time.format.{DateTimeFormatter, DateTimeParseException}
 
 case class Request(nino: String,
                    forename: String,
                    surname: String,
                    datOfBirth: LocalDate,
-                   dateRange: String)
+                   dateRange: String,
+                   correlationId: String)
+
+object Request {
+  def fromMap(citizenInfo: Map[String, String]): Option[Request] = {
+    val dobFormatter = DateTimeFormatter.ofPattern("ddMMyyyy")
+
+    for {
+      nino <- citizenInfo.get("nino")
+      forename <- citizenInfo.get("forename")
+      surname <- citizenInfo.get("surname")
+      dobAsString <- citizenInfo.get("dateOfBirth")
+      dateOfBirth <- try {
+        Some(LocalDate.parse(dobAsString, dobFormatter))
+      } catch {
+        case _: DateTimeParseException => None
+      }
+      dateRange <- citizenInfo.get("dateRange")
+      correlationId <- citizenInfo.get("correlationId")
+    } yield Request(nino, forename, surname, dateOfBirth, dateRange, correlationId)
+  }
+}
 
