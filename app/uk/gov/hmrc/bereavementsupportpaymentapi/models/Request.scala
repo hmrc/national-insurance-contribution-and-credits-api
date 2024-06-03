4e9b337e4906148de862a4462e1af41de35ababb
jacobwestwell <89075596+jacobwestwell@users.noreply.github.com>
2024-06-03 16:35:55 +0100

    Renamed packages

--- app/uk/gov/hmrc/bereavementsupportpaymentapi/models/Request.scala
+++ app/uk/gov/hmrc/bereavementsupportpaymentapi/models/Request.scala
@@ -14,22 +14,66 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.bereavementsupportpaymentapi.models
+package uk.gov.hmrc.nationalinsurancecontributionandcreditsapi.models
 import com.google.inject.Inject
-import play.api.libs.json.{Json, OFormat}
+import uk.gov.hmrc.nationalinsurancecontributionandcreditsapi.utils.{AdditionalHeaderNames, RequestParams, Validator}
+import java.time.LocalDate
+import java.time.format.{DateTimeFormatter, DateTimeParseException}
 
 
-case class Request(nationalInsuranceNumber: String,
-                   startTaxYear: Int,
-                   endTaxYear: Int,
-                   dateOfBirth: String = "")
-
-
+case class Request(nino: String,
+                   forename: String,
+                   surname: String,
+                   dateOfBirth: String,
+                   dateRange: String,
+                   correlationId: String)
 @Inject
 object Request {
-  implicit val format: OFormat[Request] = Json.format[Request]
+  def buildRequestFromMap(citizenInfo: Map[String, String ]): Option[Request] = {
+    val dobFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
+    val queryParams: Map[String, String] = validateParams(citizenInfo)
 
+    for {
+      nino <- queryParams.get(RequestParams.NINO)
+      forename <- queryParams.get(RequestParams.FORENAME)
+      surname <- queryParams.get(RequestParams.SURNAME)
+      dobAsString <- queryParams.get(RequestParams.DATE_OF_BIRTH)
+      dateOfBirth <- try {
+        LocalDate.parse(dobAsString, dobFormatter)
+        queryParams.get(RequestParams.DATE_OF_BIRTH)
+      } catch {
+        case _: DateTimeParseException => None
+      }
+      dateRange <- queryParams.get(RequestParams.DATE_RANGE)
+      correlationId <- queryParams.get(AdditionalHeaderNames.CORRELATION_ID)
+    } yield Request(nino, forename, surname, dobAsString, dateRange, correlationId)
+  }
 
+  private def validateParams(flattenedQueryParams: Map[String, String]): Map[String, String] = {
+    val validator = new Validator()
+
+    val validatedFlatQueryParams: Map[String, String] = flattenedQueryParams.flatMap{
+      case (RequestParams.NINO, value) =>
+        validator.ninoValidator(value).map( value => RequestParams.NINO -> value)
+      case (RequestParams.FORENAME, value) =>
+        validator.textValidator(value).map ( value => RequestParams.FORENAME -> value)
+      case (RequestParams.SURNAME, value) =>
+        validator.textValidator(value).map ( value => RequestParams.SURNAME -> value)
+      case (RequestParams.DATE_OF_BIRTH, value) =>
+        validator.dobValidator(value).map ( value => RequestParams.DATE_OF_BIRTH -> value.toString )
+      case (RequestParams.DATE_RANGE, value) =>
+        validator.textValidator(value).map ( value => RequestParams.DATE_RANGE -> value )
+      case (AdditionalHeaderNames.CORRELATION_ID, value) =>  Option((AdditionalHeaderNames.CORRELATION_ID, value): (String, String))
+      case _ => None //todo: throw exception with status code for any other parameter sent not expected
+    }
+
+    validatedFlatQueryParams.map {
+      case (key: String, value: String) => {
+        key -> value
+      }
+    }.toMap
+
+  }
 }
 
