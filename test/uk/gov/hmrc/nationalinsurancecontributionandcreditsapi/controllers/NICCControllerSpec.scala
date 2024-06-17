6af29ade09cf4e5af40c5f05e32ea6d2ff09b83d
jacobwestwell <89075596+jacobwestwell@users.noreply.github.com>
2024-06-17 14:08:45 +0100

    Added request body json

--- test/uk/gov/hmrc/nationalinsurancecontributionandcreditsapi/controllers/NICCControllerSpec.scala
+++ test/uk/gov/hmrc/nationalinsurancecontributionandcreditsapi/controllers/NICCControllerSpec.scala
@@ -23,6 +23,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar.mock
 import play.api.http.Status
+import play.api.libs.json.Json
 import play.api.test.Helpers.{CONTENT_TYPE, defaultAwaitTimeout, status}
 import play.api.test.{FakeHeaders, FakeRequest, Helpers}
 import uk.gov.hmrc.domain.Nino
@@ -35,24 +36,23 @@ import scala.concurrent.{ExecutionContextExecutor, Future}
 class NICCControllerSpec extends AnyWordSpec with Matchers {
 
   implicit val executor: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
-  implicit val materializer = Materializer(ActorSystem())
+  implicit val materializer: Materializer = Materializer(ActorSystem())
 
 
   "GET /nationalInsuranceNumber-info" should {
     "return 200" in {
-      val requestNino = Nino("AA123456B")
+      val requestNino = Nino("BB000020B")
       val requestStartYear = "2017"
       val requestEndYear = "2019"
       val requestDoB = "1998-04-23"
-      val requestDoBJson = "{\"dateOfBirth\": \"1998-04-23\"}"
+      val body = Json.obj(("dateOfBirth", "1998-04-23"))
       val requestObject = NICCRequest(requestNino, requestStartYear, requestEndYear, requestDoB)
-      val fakeRequest = FakeRequest[String]("POST", "/nicc-json-service/nicc/v1/api/national-insurance/AA123456B/from/2017/to/2019", FakeHeaders(Seq((CONTENT_TYPE, "application/json"))), body = requestDoBJson)
+      val fakeRequest = FakeRequest("POST", "/nicc-json-service/nicc/v1/api/national-insurance/AA123456B/from/2017/to/2019", FakeHeaders(Seq((CONTENT_TYPE, "application/json"))), body)
 
       val mockHipConnector: HipConnector = mock[HipConnector]
       val expectedResponseObject: HIPOutcome = Right(NICCResponse(Seq(NIContribution(2018, "A", "A", BigDecimal(1), BigDecimal(1), "A", BigDecimal(1))), Seq()))
       when(mockHipConnector.fetchData(requestObject)(HeaderCarrier())).thenReturn(Future(expectedResponseObject))
       val controller = new NICCController(Helpers.stubControllerComponents(), mockHipConnector)
-
 
       val response = controller.getContributionsAndCredits(requestNino, requestStartYear, requestEndYear)(fakeRequest)
       status(response) shouldBe Status.OK
