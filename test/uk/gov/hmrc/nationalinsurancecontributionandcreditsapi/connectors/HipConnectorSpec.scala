bad7a9f60ec596c74aa4c2c857c342bf71fb8c7e
MartinTGeorgiev <33837712+MartinTGeorgiev@users.noreply.github.com>
2024-07-16 14:58:56 +0100

    BDS-17 - Fixed unit tests

--- test/uk/gov/hmrc/nationalinsurancecontributionandcreditsapi/connectors/HipConnectorSpec.scala
+++ test/uk/gov/hmrc/nationalinsurancecontributionandcreditsapi/connectors/HipConnectorSpec.scala
@@ -46,7 +46,7 @@ class HipConnectorSpec extends AnyWordSpec with GuiceOneAppPerSuite with WireMoc
       "200 is returned from downstream" in {
         val payload = NICCRequest(NICCNino("BB000200B"), "2013", "2015", "1998-03-23")
 
-        stubPostServer(aResponse().withStatus(200), s"/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
+        stubPostServer(aResponse().withStatus(200), s"/nps/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
         await(connector.fetchData(payload)).status shouldBe OK
       }
     }
@@ -54,20 +54,20 @@ class HipConnectorSpec extends AnyWordSpec with GuiceOneAppPerSuite with WireMoc
       "400 is returned from downstream" in {
         val payload = NICCRequest(NICCNino("BB000400B"), "2013", "2015", "1998-03-23")
 
-        stubPostServer(aResponse().withStatus(400), s"/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
+        stubPostServer(aResponse().withStatus(400), s"/nps/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
         await(connector.fetchData(payload)).status shouldBe BAD_REQUEST
       }
 
       "403 is returned from downstream" in {
         val payload = NICCRequest(NICCNino("BB000403B"), "2013", "2015", "1998-03-23")
 
-        stubPostServer(aResponse().withStatus(403), s"/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
+        stubPostServer(aResponse().withStatus(403), s"/nps/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
         await(connector.fetchData(payload)).status shouldBe FORBIDDEN
       }
       "422 is returned from downstream" in {
         val payload = NICCRequest(NICCNino("BB000422B"), "2013", "2015", "1998-03-23")
 
-        stubPostServer(aResponse().withStatus(422), s"/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
+        stubPostServer(aResponse().withStatus(422), s"/nps/nps-json-service/nps/v1/api/national-insurance/${payload.nationalInsuranceNumber.nino}/contributions-and-credits/from/${payload.startTaxYear}/to/${payload.endTaxYear}")
         await(connector.fetchData(payload)).status shouldBe UNPROCESSABLE_ENTITY
       }
     }
