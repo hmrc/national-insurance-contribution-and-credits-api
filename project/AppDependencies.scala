defbd4c7237ab4e8156986f2042be0b9197c030a
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2025-04-01 15:25:40 +0100

    Remove unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,14 +8,12 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc"             %% "domain-play-30"             % "10.0.0",
-    "uk.gov.hmrc"             %% "play-auditing-play-30"      % "9.0.0",
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
     "org.scalamock"           %% "scalamock"                  % "7.3.0"                     % Test,
     "org.scalatest"           %% "scalatest"                  % "3.2.19"                    % Test,
-
   )
 
   val it = Seq.empty
