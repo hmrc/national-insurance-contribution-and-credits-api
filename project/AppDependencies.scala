2c2d64d417501d5a62a6904e82ba36125de729bc
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2025-04-01 14:22:54 +0100

    Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.11.0"
 
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc"             %% "domain-play-30"             % "9.0.0",
+    "uk.gov.hmrc"             %% "domain-play-30"             % "10.0.0",
     "uk.gov.hmrc"             %% "play-auditing-play-30"      % "9.0.0",
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "org.scalamock"           %% "scalamock"                  % "6.0.0"                     % Test,
-    "org.scalatest"           %% "scalatest"                  % "3.2.18"                    % Test,
+    "org.scalamock"           %% "scalamock"                  % "7.3.0"                     % Test,
+    "org.scalatest"           %% "scalatest"                  % "3.2.19"                    % Test,
 
   )
 
